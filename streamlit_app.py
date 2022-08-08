e1412cf3d64f47738d4b091807ef82d51583469c
Riaan Jacobsz <110789945+riaanjacobszsf@users.noreply.github.com>
2022-08-08 15:45:00 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,10 @@ def get_fruitvice_data(this_fruit_choice):
 streamlit.header('Fruitvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like the information about?')
-    if not this_fruit_choice:
+    if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        back_from_function = get_fruityvice_data(this_fruit_choice)
+        back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
   
 except URLError as e:
